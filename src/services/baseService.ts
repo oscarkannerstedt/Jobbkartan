4c9d0f719b9088ce900a9f0df760bd549e8c335a
oscarkannerstedt <112692728+oscarkannerstedt@users.noreply.github.com>
2024-09-26 14:33:23 +0200

    ✨Added - fetchAllJobs from api

--- src/services/baseService.ts
+++ src/services/baseService.ts
@@ -1,17 +1,35 @@
 import axios from "axios";
 
+const BASE_URL = "https://jobsearch.api.jobtechdev.se/search";
+
+//Hämtar alla annonser.
+export const fetchAllJobs = async () => {
+  try {
+    const response = await axios.get(BASE_URL, {
+      params: {
+        offset: 0,
+        limit: 100,
+        q: "",
+      },
+    });
+
+    return response.data;
+  } catch (error) {
+    console.error("Error while fetching from api: ", error);
+    throw error;
+  }
+};
+
 //Hämtar jobb annonser efter vad man sökt på.
 export const fetchJobsBySearchTerm = async (searchTerm: string) => {
   try {
-    const response = await axios.get(
-      "https://jobsearch.api.jobtechdev.se/search",
-      {
-        params: {
-          offset: 0,
-          q: searchTerm,
-        },
-      }
-    );
+    const response = await axios.get(BASE_URL, {
+      params: {
+        offset: 0,
+        limit: 100,
+        q: searchTerm,
+      },
+    });
 
     return response.data;
   } catch (error) {
