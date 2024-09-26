31bec682bc1470c30e1972d8f309344cc5bebe45
CarolineVarsaga <carolinevarsaga@gmail.com>
2024-09-26 13:23:37 +0200

    :sparkles: sets up router

---
+++ src/Router.tsx
@@ -0,0 +1,28 @@
+import { createHashRouter } from "react-router-dom";
+import Layout from "./pages/Layout";
+import Home from "./pages/Home";
+import SearchResults from "./pages/SearchResults";
+import SelectedJob from "./pages/SelectedJob";
+import NotFound from "./pages/NotFound";
+
+export const router = createHashRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    errorElement: <NotFound />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/annonser",
+        element: <SearchResults />,
+      },
+      {
+        path: "/annonser/:id",
+        element: <SelectedJob />,
+      },
+    ],
+  },
+]);
