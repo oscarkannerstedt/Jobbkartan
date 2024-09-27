52099a81926147ccc911032382cb81250096016c
Annika-Nyholm <146995394+Annika-Nyholm@users.noreply.github.com>
2024-09-27 15:18:41 +0200

    🐛

--- src/services/mapService.ts
+++ src/services/mapService.ts
@@ -3,6 +3,3 @@ const apiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
 export const generateMapUrl = (lat: number, lng: number): string => {
 	return `https://www.google.com/maps/embed/v1/place?key=${apiKey}&q=${lat},${lng}`;
 };
-// export const generateMapUrl = () => {
-//     return 'https://www.google.com/maps/embed/v1/place?key=AIzaSyAf8aR3sEp5wBFYnffTwMo8RaOvDZq80HA&q=59.3293,18.0686';
-// }
