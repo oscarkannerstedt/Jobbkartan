5236e0bc5e070b9374c6f358eb915ba020856743
CarolineVarsaga <carolinevarsaga@gmail.com>
2024-09-27 11:27:11 +0200

    :sparkles: adds digilayoutblock for margin and padding

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,3 @@
-//import { useState } from "react";
 import {
   LayoutBlockContainer,
   LayoutBlockVariation,
@@ -8,64 +7,45 @@ import {
   DigiHeaderNavigation,
   DigiHeaderNavigationItem,
   DigiLayoutBlock,
-  DigiUtilBreakpointObserver,
 } from "@digi/arbetsformedlingen-react";
 
 const Header = () => {
-  // const [isLarge, setIsLarge] = useState(true);
-
-  // const handleLargeBreakpoint = () => {
-  //   setIsLarge(true);
-  // };
-
-  // const handleSmallBreakpoint = () => {
-  //   setIsLarge(false);
-  // };
-
   return (
     <>
-      <DigiUtilBreakpointObserver
-      // onAfOnLarge={handleLargeBreakpoint}
-      // onAfOnSmall={handleSmallBreakpoint}
+      <DigiLayoutBlock
+        afVariation={LayoutBlockVariation.PRIMARY}
+        afContainer={LayoutBlockContainer.STATIC}
       >
-        <DigiLayoutBlock
-          afVariation={LayoutBlockVariation.PRIMARY}
-          afContainer={LayoutBlockContainer.NONE}
+        <DigiHeader
+          afSystemName="Jobbkartan"
+          afHideSystemName={false}
+          afMenuButtonText="Meny"
         >
-          <DigiHeader
-            afSystemName="Jobbkartan"
-            afHideSystemName={false}
-            afMenuButtonText="Meny"
-          >
-            <a
-              slot="header-logo"
-              aria-label="Jobbkartans startsida"
-              href="/"
-            ></a>
-            <div slot="header-content"></div>
-            <div slot="header-navigation">
-              <DigiHeaderNavigation
-                afCloseButtonText="Stäng"
-                afCloseButtonAriaLabel="Stäng meny"
-                afNavAriaLabel="Huvudmeny"
-              >
-                <DigiHeaderNavigationItem afCurrentPage={true}>
-                  <a href="/">Start</a>
-                </DigiHeaderNavigationItem>
-                <DigiHeaderNavigationItem>
-                  <a href="/">Mina sidor</a>
-                </DigiHeaderNavigationItem>
-                <DigiHeaderNavigationItem>
-                  <a href="/">Sök jobb i Jobbkartan</a>
-                </DigiHeaderNavigationItem>
-                <DigiHeaderNavigationItem>
-                  <a href="/">Other languages</a>
-                </DigiHeaderNavigationItem>
-              </DigiHeaderNavigation>
-            </div>
-          </DigiHeader>
-        </DigiLayoutBlock>
-      </DigiUtilBreakpointObserver>
+          <a slot="header-logo" aria-label="Jobbkartans startsida" href="/"></a>
+
+          <div slot="header-content"></div>
+          <div slot="header-navigation">
+            <DigiHeaderNavigation
+              afCloseButtonText="Stäng"
+              afCloseButtonAriaLabel="Stäng meny"
+              afNavAriaLabel="Huvudmeny"
+            >
+              <DigiHeaderNavigationItem afCurrentPage={true}>
+                <a href="/">Start</a>
+              </DigiHeaderNavigationItem>
+              <DigiHeaderNavigationItem>
+                <a href="/">Mina sidor</a>
+              </DigiHeaderNavigationItem>
+              <DigiHeaderNavigationItem>
+                <a href="/">Sök jobb i Jobbkartan</a>
+              </DigiHeaderNavigationItem>
+              <DigiHeaderNavigationItem>
+                <a href="/">Other languages</a>
+              </DigiHeaderNavigationItem>
+            </DigiHeaderNavigation>
+          </div>
+        </DigiHeader>
+      </DigiLayoutBlock>
     </>
   );
 };
