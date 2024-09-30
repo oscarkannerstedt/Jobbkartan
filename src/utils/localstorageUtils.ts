7d0630349b6f1db5aad973368b8da4456d7147d3
CarolineVarsaga <carolinevarsaga@gmail.com>
2024-09-30 13:58:15 +0200

    :sparkles: local storage utils

--- src/utils/localstorageUtils.ts
+++ src/utils/localstorageUtils.ts
@@ -1,4 +1,4 @@
-import { IJob } from "../models/IJobs";
+import { IJob } from "../models/IJob";
 
 export const getJobsFromLocalStorage = (): IJob[] => {
   const storedJobs = localStorage.getItem("jobs");
