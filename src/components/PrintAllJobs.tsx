492105816773d7657d56cd081590267ac290374b
oscarkannerstedt <112692728+oscarkannerstedt@users.noreply.github.com>
2024-09-30 11:49:17 +0200

    ✨ Added - formatPublicationDate

--- src/components/PrintAllJobs.tsx
+++ src/components/PrintAllJobs.tsx
@@ -1,38 +1,6 @@
 import { useContext } from "react";
 import { jobContext } from "../services/jobContext";
-
-//Helper function to format publication Date
-const formatPublicationDate = (dateString: string): string => {
-  const publicationDate = new Date(dateString);
-  const now = new Date();
-
-  const today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
-  const yesterday = new Date(today);
-  yesterday.setDate(today.getDate() - 1);
-
-  const formattedTime = `${publicationDate
-    .getHours()
-    .toString()
-    .padStart(2, "0")}:${publicationDate
-    .getMinutes()
-    .toString()
-    .padStart(2, "0")}`;
-
-  if (publicationDate >= today) {
-    return `Publicerad idag, kl. ${formattedTime}`;
-  } else if (publicationDate >= yesterday) {
-    return `Publicerad igår, kl. ${formattedTime}`;
-  } else {
-    const options: Intl.DateTimeFormatOptions = {
-      day: "numeric",
-      month: "long",
-    };
-    return `Publicerad ${publicationDate.toLocaleDateString(
-      "sv-SE",
-      options
-    )}, ${formattedTime}`;
-  }
-};
+import { formatPublicationDate } from "../utils/dateUtils/formatPublicationDate";
 
 export const PrintAllJobs = () => {
   const context = useContext(jobContext);
---
+++ src/utils/dateUtils/formatPublicationDate.ts
@@ -0,0 +1,32 @@
+//Helper function to format publication Date
+export const formatPublicationDate = (dateString: string): string => {
+  const publicationDate = new Date(dateString);
+  const now = new Date();
+
+  const today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
+  const yesterday = new Date(today);
+  yesterday.setDate(today.getDate() - 1);
+
+  const formattedTime = `${publicationDate
+    .getHours()
+    .toString()
+    .padStart(2, "0")}:${publicationDate
+    .getMinutes()
+    .toString()
+    .padStart(2, "0")}`;
+
+  if (publicationDate >= today) {
+    return `Publicerad idag, kl. ${formattedTime}`;
+  } else if (publicationDate >= yesterday) {
+    return `Publicerad igår, kl. ${formattedTime}`;
+  } else {
+    const options: Intl.DateTimeFormatOptions = {
+      day: "numeric",
+      month: "long",
+    };
+    return `Publicerad ${publicationDate.toLocaleDateString(
+      "sv-SE",
+      options
+    )}, ${formattedTime}`;
+  }
+};
