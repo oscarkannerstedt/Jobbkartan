e335dda9d0d66e3a0e616460d75fabf2d2a2a68b
Annika-Nyholm <146995394+Annika-Nyholm@users.noreply.github.com>
2024-09-30 16:44:34 +0200

    ✨ ➕ add APIProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
-import { RouterProvider } from "react-router-dom";
-import { router } from "./Router";
+import { APIProvider } from '@vis.gl/react-google-maps';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './Router';
+import { JobMap } from './components/JobMap';
+
+const apiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
 
 function App() {
-  return (
-    <>
-      <RouterProvider router={router}></RouterProvider>
-    </>
-  );
+	return (
+		<>
+			<APIProvider
+				apiKey={apiKey}
+				onLoad={() => console.log('Maps API has loaded.')}
+			>
+        <JobMap></JobMap>
+			  <RouterProvider router={router}></RouterProvider>
+			</APIProvider>
+		</>
+	);
 }
 
 export default App;
