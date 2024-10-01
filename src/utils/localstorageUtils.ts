167ad580f9d58ecbd8c3e9ca4cffa95b22e6e583
CarolineVarsaga <carolinevarsaga@gmail.com>
2024-10-01 10:01:39 +0200

    :fire: removes console log

--- src/utils/localstorageUtils.ts
+++ src/utils/localstorageUtils.ts
@@ -2,6 +2,5 @@ import { IJob } from "../models/IJob";
 
 export const getJobsFromLocalStorage = (): IJob[] => {
   const storedJobs = localStorage.getItem("jobs");
-  console.log("Data från localStorage:", storedJobs);
   return storedJobs ? JSON.parse(storedJobs) : [];
 };
