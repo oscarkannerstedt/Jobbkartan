d3320cfe9b7cc944cd6e7413a8a782810f207628
Annika-Nyholm <146995394+Annika-Nyholm@users.noreply.github.com>
2024-10-01 10:17:03 +0200

    ✨ add clusterer

--- src/components/PoiMarkers.tsx
+++ src/components/PoiMarkers.tsx
@@ -1,19 +1,62 @@
-import { AdvancedMarker, Pin } from "@vis.gl/react-google-maps";
-import { PointOfInterest } from "../models/PointOfInterest"; 
+import { useEffect, useRef, useState } from 'react';
+import { AdvancedMarker, Pin, useMap } from '@vis.gl/react-google-maps';
+import { PointOfInterest } from '../models/PointOfInterest';
+import { MarkerClusterer } from '@googlemaps/markerclusterer';
+import type { Marker } from '@googlemaps/markerclusterer';
 
-
-// To mark with pins of map02
+// To mark with pins of map
 // adjust whenJobAds are implemented
-export const PoiMarkers = (props: {pois: PointOfInterest[]}) => {
-    return (
-      <>
-        {props.pois.map( (poi: PointOfInterest) => (
-          <AdvancedMarker
-            key={poi.key}
-            position={poi.location}>
-          <Pin background={'#a00eda'} glyphColor={'#000'} borderColor={'#000'} />
-          </AdvancedMarker>
-        ))}
-      </>
-    );
-  };
+export const PoiMarkers = (props: { pois: PointOfInterest[] }) => {
+	const map = useMap();
+	const [markers, setMarkers] = useState<{ [key: string]: Marker }>({});
+	const clusterer = useRef<MarkerClusterer | null>(null);
+
+	useEffect(() => {
+		if (!map) return;
+
+		if (!clusterer.current) {
+			clusterer.current = new MarkerClusterer({ map });
+		}
+	}, [map]);
+
+	useEffect(() => {
+		if (clusterer.current) {
+			const markerArray = Object.values(markers);
+			clusterer.current.clearMarkers();
+			clusterer.current.addMarkers(markerArray);
+		}
+	}, [markers]);
+
+	const setMarkerRef = (marker: Marker | null, key: string) => {
+		if (marker && markers[key]) return;
+		if (!marker && !markers[key]) return;
+
+		setMarkers((prev) => {
+			if (marker) {
+				return { ...prev, [key]: marker };
+			} else {
+				const newMarkers = { ...prev };
+				delete newMarkers[key];
+				return newMarkers;
+			}
+		});
+	};
+
+	return (
+		<>
+			{props.pois.map((poi: PointOfInterest) => (
+				<AdvancedMarker
+					key={poi.key}
+					position={poi.location}
+					ref={(marker) => setMarkerRef(marker, poi.key)}
+				>
+					<Pin
+						background={'#a00eda'}
+						glyphColor={'#000'}
+						borderColor={'#000'}
+					/>
+				</AdvancedMarker>
+			))}
+		</>
+	);
+};
