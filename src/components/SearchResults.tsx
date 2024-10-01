1fcc2d64b976ae9f473adc750dd5c3fdc20405ed
oscarkannerstedt <112692728+oscarkannerstedt@users.noreply.github.com>
2024-10-01 14:17:59 +0200

    Added - search job context

--- src/components/SearchResults.tsx
+++ src/components/SearchResults.tsx
@@ -1,9 +1,1 @@
-import { createContext } from "react";
-import { IJob } from "../models/IJob";
 
-interface ISearchJobContext {
-  jobs: IJob[];
-  fetchJobsBySearchTerm: (searchTerm: string) => Promise<void>;
-}
-
-export const searchJobContext = createContext<ISearchJobContext | null>(null);
