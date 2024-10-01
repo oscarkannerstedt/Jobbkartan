ec372eb85052e0ddd1d0a6f9e191144c2baf320c
CarolineVarsaga <carolinevarsaga@gmail.com>
2024-10-01 11:56:21 +0200

    :sparkles: the title gets the  same name as job heading

--- src/pages/SelectedJob.tsx
+++ src/pages/SelectedJob.tsx
@@ -3,9 +3,22 @@ import { useLoaderData } from "react-router-dom";
 import { IJob } from "../models/IJob";
 import { DigiLayoutBlock } from "@digi/arbetsformedlingen-react";
 import BackButton from "../components/BackButton";
+import { useEffect } from "react";
 
 const SelectedJob = () => {
   const currentJob = useLoaderData() as IJob | null;
+
+  useEffect(() => {
+    if (currentJob) {
+      document.title = `${currentJob.headline}`;
+    } else {
+      document.title = "Kunde inte hitta annonsen";
+    }
+    return () => {
+      document.title = "Jobbkartan";
+    };
+  }, [currentJob]);
+
   return (
     <>
       <DigiLayoutBlock>
