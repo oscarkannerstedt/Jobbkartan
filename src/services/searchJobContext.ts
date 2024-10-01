73f101cc425f8852c72da1e4ca16c29fb28f9614
oscarkannerstedt <112692728+oscarkannerstedt@users.noreply.github.com>
2024-10-01 14:45:00 +0200

    Added - job provider by search term

--- src/services/searchJobContext.ts
+++ src/services/searchJobContext.ts
@@ -3,7 +3,7 @@ import { IJob } from "../models/IJob";
 
 interface ISearchJobContext {
   jobs: IJob[];
-  fetchJobsBySearchTerm: (searchTerm: string) => Promise<void>;
+  fetchJobs: (searchTerm: string) => Promise<void>;
 }
 
 export const searchJobContext = createContext<ISearchJobContext | null>(null);
