f1b727fbe0f28f0e601f3c3572608f3927702a49
CarolineVarsaga <carolinevarsaga@gmail.com>
2024-10-01 08:48:22 +0200

    :fire: removes test code

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,24 +1,5 @@
-import ShowJob from "../components/ShowJob";
-import { useLoaderData } from "react-router-dom";
-import { IJob } from "../models/IJob";
-import { DigiLayoutBlock } from "@digi/arbetsformedlingen-react";
-import BackButton from "../components/BackButton";
-
 const Home = () => {
-  const currentJob = useLoaderData() as IJob | null;
-
-  return (
-    <>
-      <DigiLayoutBlock>
-        <BackButton />
-        {currentJob ? (
-          <ShowJob job={currentJob} />
-        ) : (
-          <p>Kunde inte hitta annonsen</p>
-        )}
-      </DigiLayoutBlock>
-    </>
-  );
+  return <></>;
 };
 
 export default Home;
