70868d3f154b8a423b7b4814db6d457eb9676746
CarolineVarsaga <carolinevarsaga@gmail.com>
2024-10-01 10:04:56 +0200

    ♻️ changes DigiLink to Link from react-router-dom

--- src/components/PrintAllJobs.tsx
+++ src/components/PrintAllJobs.tsx
@@ -4,58 +4,60 @@ import { formatPublicationDate } from "../utils/dateUtils/formatPublicationDate"
 import {
   DigiLayoutBlock,
   DigiLayoutContainer,
-  DigiLink,
+  DigiTypography,
 } from "@digi/arbetsformedlingen-react";
 import {
   LayoutBlockContainer,
   LayoutBlockVariation,
 } from "@digi/arbetsformedlingen";
 import "../styles/printAllJobb.css";
+import { Link } from "react-router-dom";
 
 export const PrintAllJobs = () => {
   const context = useContext(jobContext);
 
-  if (!context) return <p>Loading...</p>;
+  if (!context) return <p>Laddar...</p>;
 
   const { jobs } = context;
 
   return (
-    <DigiLayoutContainer>
-      <div>
-        {jobs.length > 0 ? (
-          jobs.map((job) => (
-            <DigiLayoutBlock
-              key={job.id}
-              afVariation={LayoutBlockVariation.PRIMARY}
-              afContainer={LayoutBlockContainer.FLUID}
-              afMarginBottom={false}
-              className="digiLayoutBlock"
-            >
-              <h3 style={{ paddingTop: "15px" }}>
-                <DigiLink
-                  afHref={`/annonser/${job.id}`}
-                  afVariation="large"
-                  afAriaLabel={`Gå till annons för ${job.headline} hos ${job.employer.name} i ${job.workplace_address.municipality}`}
-                >
-                  {job.headline}
-                </DigiLink>
-              </h3>
+    <DigiTypography>
+      <DigiLayoutContainer>
+        <div>
+          {jobs.length > 0 ? (
+            jobs.map((job) => (
+              <DigiLayoutBlock
+                key={job.id}
+                afVariation={LayoutBlockVariation.PRIMARY}
+                afContainer={LayoutBlockContainer.FLUID}
+                afMarginBottom={false}
+                className="digiLayoutBlock"
+              >
+                <h3 style={{ paddingTop: "15px" }}>
+                  <Link
+                    to={`/annonser/${job.id}`}
+                    aria-label={`Gå till annons för ${job.headline} hos ${job.employer.name} i ${job.workplace_address.municipality}`}
+                  >
+                    {job.headline}
+                  </Link>
+                </h3>
 
-              <h4>
-                {job.employer.name} - {job.workplace_address.municipality}
-              </h4>
-              <p style={{ margin: 0, lineHeight: ".1" }}>
-                {job.occupation.label}
-              </p>
-              <p style={{ paddingBottom: "15px" }}>
-                {formatPublicationDate(job.publication_date)}
-              </p>
-            </DigiLayoutBlock>
-          ))
-        ) : (
-          <p>Inga jobb tillgängliga...</p>
-        )}
-      </div>
-    </DigiLayoutContainer>
+                <h4>
+                  {job.employer.name} - {job.workplace_address.municipality}
+                </h4>
+                <p style={{ margin: 0, lineHeight: ".1" }}>
+                  {job.occupation.label}
+                </p>
+                <p style={{ paddingBottom: "15px" }}>
+                  {formatPublicationDate(job.publication_date)}
+                </p>
+              </DigiLayoutBlock>
+            ))
+          ) : (
+            <p>Inga jobb tillgängliga...</p>
+          )}
+        </div>
+      </DigiLayoutContainer>
+    </DigiTypography>
   );
 };
