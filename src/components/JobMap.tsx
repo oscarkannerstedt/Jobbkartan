35d319f965d7fa83b9e55937809675f64c0f2866
Annika-Nyholm <146995394+Annika-Nyholm@users.noreply.github.com>
2024-10-01 18:24:52 +0200

    ✨ added geocoding functionality

--- src/components/JobMap.tsx
+++ src/components/JobMap.tsx
@@ -3,49 +3,130 @@ import { Map, MapCameraChangedEvent } from '@vis.gl/react-google-maps';
 import { PoiMarkers } from './PoiMarkers';
 import { JobInfoWindow } from '../models/JobInfoWindow';
 import { calculateCenter } from '../utils/calculateCenter';
-//import { PointOfInterest } from '../models/PointOfInterest';
-
-// until function for platsbankens api
-// const locations: PointOfInterest[] = [
-// 	{key: 'Tändsticksmuseet', location: {lat: 57.78512, lng: 14.15968}},
-// 	{key: 'Nyholmska hemmet', location: {lat: 57.79463, lng: 14.11966}},
-// 	{key: 'Asecs köpcenter', location: {lat: 57.77353, lng: 14.20454}},
-// 	{key: 'ICA Maxi', location: {lat: 57.77244, lng: 14.17647}},
-// 	{key: 'Bryggan Café och Bistro', location: {lat: 57.78139, lng: 14.17068}}
-// ]
+import { useEffect, useState } from 'react';
+import { geocodeAddress } from '../services/geocodeService';
+import { IJob } from '../models/IJob';
 
 export const JobMap = () => {
 	const { jobs } = useJobs();
+	const [jobLocations, setJobLocations] = useState<JobInfoWindow[]>([]);
+	const [loading, setLoading] = useState<boolean>(true);
+	const [error, setError] = useState<string | null>(null);
 
-	console.log('hämtade jobs', jobs);
-
-	const jobLocations: JobInfoWindow[] = jobs
-		.filter((job) => {
-			const coords = job.workplace_address.coordinates;
-			return (
-				coords &&
-				coords.length === 2 &&
-				!isNaN(coords[0]) &&
-				!isNaN(coords[1])
+	useEffect(() => {
+		const fetchJobLocations = async () => {
+			setLoading(true);
+			setError(null);
+			const locations = await Promise.all(
+				jobs.map(async (job) => await getJobLocation(job))
 			);
-		})
-		.map((job) => ({
-			id: job.id,
-			headline: job.headline,
-			employerName: job.employer.name,
-			address: job.workplace_address.street_address,
-			city: job.workplace_address.city,
-			coordinates: {
-				lat: Number(job.workplace_address.coordinates[1]),
-				lng: Number(job.workplace_address.coordinates[0]),
-			},
-		}));
-	console.log('Job locations', jobLocations);
+
+			const validLocations = locations.filter(
+				(location): location is JobInfoWindow =>
+					location !== null &&
+					location.coordinates.lat !== 0 &&
+					location.coordinates.lng !== 0
+			);
+
+			console.log('Valid Locations:', validLocations);
+
+			setJobLocations(validLocations);
+			setLoading(false);
+		};
+
+		fetchJobLocations();
+	}, [jobs]);
+
+	const getJobLocation = async (job: IJob): Promise<JobInfoWindow | null> => {
+		const {
+			street_address: streetAddress,
+			city,
+			coordinates,
+		} = job.workplace_address;
+
+		if (coordinates && coordinates.length === 2) {
+			const lat = Number(coordinates[1]);
+			const lng = Number(coordinates[0]);
+			if (lat !== 0 && lng !== 0) {
+				console.log(
+					`Using existing coordinates for job ID ${job.id}:`,
+					coordinates
+				);
+
+				return {
+					id: job.id,
+					headline: job.headline,
+					employerName: job.employer.name,
+					address: streetAddress,
+					city: city,
+					coordinates: {
+						lat: lat,
+						lng: lng,
+					},
+				};
+			} else {
+				console.warn(
+					`Invalid coordinates for job ID ${job.id}: lat=${lat}, lng=${lng}`
+				);
+				return null;
+			}
+		}
+
+		if (!streetAddress || !city) {
+			console.warn(
+				`Invalid address for job ID ${job.id}: streetAddress or city is missing.`
+			);
+			return null;
+		}
+
+		// Geocoding
+		try {
+			const location = await geocodeAddress(`${streetAddress}, ${city}`);
+
+			if (!location) {
+				console.warn(
+					`No valid address found for ${streetAddress}, ${city}`
+				);
+				return null;
+			}
+
+			const lat = location.lat();
+			const lng = location.lng();
+			console.log(
+				`Geocoded ${streetAddress}, ${city} to coordinates: lat=${lat}, lng=${lng}`
+			);
+
+			if (lat !== 0 && lng !== 0) {
+				return {
+					id: job.id,
+					headline: job.headline,
+					employerName: job.employer.name,
+					address: streetAddress,
+					city: city,
+					coordinates: {
+						lat: lat, // location.lat(),
+						lng: lng, // location.lng(),
+					},
+				};
+			} else {
+				console.warn(
+					`Geocoding returned invalid coordinates for ${streetAddress}, ${city}: lat=${lat}, lng=${lng}`
+				);
+				return null;
+			}
+		} catch (error) {
+			console.error('Geocode failed for address:', streetAddress, error);
+			setError(`Geocode failed for address: ${streetAddress}`);
+			return null;
+		}
+	};
 
 	const center = calculateCenter(jobLocations);
 
 	return (
 		<>
+			{loading && <div>Loading...</div>}
+			{error && <div>Error: {error}</div>}
 			<Map
 				style={{ height: 800 }}
 				defaultZoom={5}
