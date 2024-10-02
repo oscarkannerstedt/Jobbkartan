518ce7ac929a0c7166b0c5423ef15e7d197052e7
CarolineVarsaga <carolinevarsaga@gmail.com>
2024-10-02 09:00:18 +0200

    :sparkles: imports screen size provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "./Router";
 import { JobProvider } from "./services/JobProvider";
+import ScreenSizeProvider from "./services/ScreenSizeProvider";
 
 function App() {
   return (
     <>
-      <JobProvider>
-        <RouterProvider router={router}></RouterProvider>
-      </JobProvider>
+      <ScreenSizeProvider>
+        <JobProvider>
+          <RouterProvider router={router}></RouterProvider>
+        </JobProvider>
+      </ScreenSizeProvider>
     </>
   );
 }
