b8ea4c9137cd52e5873cff70e99334aa12f5d830
CarolineVarsaga <carolinevarsaga@gmail.com>
2024-10-02 15:30:23 +0200

    :tada: creats component for apply now button

---
+++ src/components/ApplyNowButton.tsx
@@ -0,0 +1,11 @@
+import { DigiButton } from "@digi/arbetsformedlingen-react";
+
+const ApplyNowButton = () => {
+  return (
+    <>
+      <DigiButton>Ansök här</DigiButton>
+    </>
+  );
+};
+
+export default ApplyNowButton;
