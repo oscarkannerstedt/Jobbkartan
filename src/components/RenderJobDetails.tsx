8c926c96132b5fd303ca228e0d532807a26c60ca
CarolineVarsaga <carolinevarsaga@gmail.com>
2024-10-03 16:12:14 +0200

    :sparkles: adds render job component

---
+++ src/components/RenderJobDetails.tsx
@@ -0,0 +1,132 @@
+import {
+  InfoCardHeadingLevel,
+  InfoCardType,
+  InfoCardVariation,
+  LayoutBlockContainer,
+  LayoutBlockVariation,
+  LinkVariation,
+} from "@digi/arbetsformedlingen";
+import {
+  DigiInfoCard,
+  DigiLayoutBlock,
+  DigiLinkExternal,
+} from "@digi/arbetsformedlingen-react";
+import LogoComponent from "./LogoComponent";
+import QualificationsWindow from "./QualificationsWindow";
+import {
+  calculateDaysLeftToDeadline,
+  formatDate,
+} from "../services/baseService";
+import ApplyNowInfo from "./ApplyNowInfo";
+import { formatPublicationDate } from "../utils/dateUtils/formatPublicationDate";
+import { IShowJobProps } from "./ShowJob";
+
+const RenderJobDetails = ({ job }: IShowJobProps) => {
+  const { municipality, street_address, postcode, city, region } =
+    job.workplace_address || {};
+  const daysLeft = calculateDaysLeftToDeadline(job.application_deadline);
+  return (
+    <>
+      <DigiLayoutBlock
+        afVariation={LayoutBlockVariation.PRIMARY}
+        afContainer={LayoutBlockContainer.STATIC}
+        className="job-container"
+      >
+        <LogoComponent job={job} />
+        <h1>{job.headline}</h1>
+        <h2>{job.employer.name}</h2>
+        <h3>{job.occupation.label}</h3>
+        <h3>Kommun: {municipality ? municipality : "Ospecifierad ort"}</h3>
+        <DigiLayoutBlock
+          className="employment-information-container"
+          afContainer={LayoutBlockContainer.NONE}
+        >
+          <p>Antal tjänster: {job.number_of_vacancies}</p>
+          <span>Omfattning: {job.working_hours_type.label}</span>
+          <span>Varaktighet: {job.duration.label}</span>
+          <span>Anställningsform: {job.employment_type.label}</span>
+        </DigiLayoutBlock>
+        <QualificationsWindow job={job} />
+        <h2>Om jobbet</h2>
+        <div
+          dangerouslySetInnerHTML={{
+            __html: job.description.text_formatted,
+          }}
+        />
+        <h2>Om Anställningen</h2>
+        <h3>Lön</h3>
+        <p>
+          <strong>{job.salary_description}</strong>
+        </p>
+        <p>
+          <strong>Lönetyp:</strong> {job.salary_type.label}
+        </p>
+        <h2>Var ligger arbetsplatsen?</h2>
+        <h3>Postadress</h3>
+        {street_address && postcode && city ? (
+          <address>
+            <div>{street_address}</div>
+            <div>
+              {postcode} {city}
+            </div>
+          </address>
+        ) : (
+          <p>
+            Arbetsplatsen ligger i kommunen <strong>{municipality}</strong> i{" "}
+            <strong>{region}</strong>.
+          </p>
+        )}
+        <h2>Om arbetsgivaren</h2>
+        <p>{job.employer.name}</p>
+        <h2>Kontakt</h2>
+        {job.employer.workplace} <br />
+        {job.employer.url && (
+          <DigiLinkExternal
+            afHref={job.employer.url}
+            afTarget="_blank"
+            afVariation={LinkVariation.SMALL}
+          >
+            {job.employer.url}
+          </DigiLinkExternal>
+        )}
+        <p>{job.employer.email}</p>
+        <p>{job.employer.phone_number}</p>
+      </DigiLayoutBlock>
+
+      <DigiLayoutBlock
+        afVariation={LayoutBlockVariation.PRIMARY}
+        afContainer={LayoutBlockContainer.STATIC}
+        className="map-container"
+      >
+        <h2>En karta här</h2>
+        <DigiInfoCard
+          afHeading="Sök jobbet"
+          afHeadingLevel={InfoCardHeadingLevel.H2}
+          afType={InfoCardType.RELATED}
+          afVariation={InfoCardVariation.SECONDARY}
+        >
+          <strong>
+            <h3>Ansök senast: {formatDate(job.application_deadline)}</h3>
+          </strong>
+          {daysLeft > 0 ? (
+            <p>
+              <em>Ansökningstiden går ut om {daysLeft} dagar</em>
+            </p>
+          ) : (
+            <p>
+              <em>Ansökningstiden har gått ut</em>
+            </p>
+          )}
+          <ApplyNowInfo job={job} />
+          <span>
+            {formatPublicationDate(job.publication_date)}
+            {". "}
+          </span>
+          <span>Annons-Id: {job.id}</span>
+        </DigiInfoCard>
+      </DigiLayoutBlock>
+    </>
+  );
+};
+
+export default RenderJobDetails;
