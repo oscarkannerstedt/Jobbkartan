c3e2d4c005978ee8a68f6e64bfba8a6057802957
Aanndreas <andreas.lundin@hotmail.com>
2024-10-06 10:46:38 +0200

    🚚 added new route-element (errorElement for 'page not found')

--- src/Router.tsx
+++ src/Router.tsx
@@ -3,28 +3,26 @@ import Layout from "./pages/Layout";
 import Home from "./pages/Home";
 import SearchResults from "./pages/SearchResults";
 import SelectedJob from "./pages/SelectedJob";
-import NotFound from "./pages/NotFound";
-//import { jobLoader } from "./loaders/jobLoader";
+import NotFound from "./components/NotFound";
 
 export const router = createHashRouter([
-  {
-    path: "/",
-    element: <Layout />,
-    errorElement: <NotFound />,
-    children: [
-      {
-        path: "/",
-        element: <Home />,
-      },
-      {
-        path: "/annonser",
-        element: <SearchResults />,
-      },
-      {
-        path: "/annonser/:id",
-        element: <SelectedJob />,
-        // loader: jobLoader,
-      },
-    ],
-  },
+	{
+		path: "/",
+		element: <Layout />,
+		errorElement: <NotFound />, // NotFound-komponent om en hemsida inte finns som route/är felaktig
+		children: [
+			{
+				path: "/",
+				element: <Home />,
+			},
+			{
+				path: "/annonser",
+				element: <SearchResults />,
+			},
+			{
+				path: "/annonser/:id",
+				element: <SelectedJob />,
+			},
+		],
+	},
 ]);
