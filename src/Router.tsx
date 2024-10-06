0d660baca7ee5d40407d097ae67ff9a5a718d5e9
Aanndreas <andreas.lundin@hotmail.com>
2024-10-06 11:06:35 +0200

    🐛 Switched from error-element to catch-all-route

--- src/Router.tsx
+++ src/Router.tsx
@@ -3,13 +3,12 @@ import Layout from "./pages/Layout";
 import Home from "./pages/Home";
 import SearchResults from "./pages/SearchResults";
 import SelectedJob from "./pages/SelectedJob";
-import NotFound from "./components/NotFound";
+import NotFound from "./components/NotFound"; // NotFound-komponent
 
 export const router = createHashRouter([
 	{
 		path: "/",
 		element: <Layout />,
-		errorElement: <NotFound />, // NotFound-komponent om en hemsida inte finns som route/är felaktig
 		children: [
 			{
 				path: "/",
@@ -22,6 +21,11 @@ export const router = createHashRouter([
 			{
 				path: "/annonser/:id",
 				element: <SelectedJob />,
+			},
+			// Catch-all route för alla icke-matchande rutter
+			{
+				path: "*",
+				element: <NotFound />, // Laddar NotFound-komponent om ingen annan route matchar
 			},
 		],
 	},
