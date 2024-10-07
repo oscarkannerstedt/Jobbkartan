256025198a3cdbbe80755cd5241f79570286722c
CarolineVarsaga <carolinevarsaga@gmail.com>
2024-10-07 08:45:41 +0200

    :sparkles: adds useEffect

--- src/hooks/usePagination.ts
+++ src/hooks/usePagination.ts
@@ -1,5 +1,6 @@
-import { useState, useContext } from "react";
+import { useState, useContext, useEffect } from "react";
 import { jobContext } from "../services/jobContext";
+//import { IJob } from "../models/IJob";
 
 const usePagination = (itemsPerPage: number) => {
   const context = useContext(jobContext);
@@ -7,12 +8,20 @@ const usePagination = (itemsPerPage: number) => {
   if (!context)
     throw new Error("usePagination must be used within a JobProvider");
 
-  const { jobs, loading } = context;
+  const { jobs, fetchJobs, loading } = context;
   const [currentPage, setCurrentPage] = useState(1);
+
+  useEffect(() => {
+    if (!loading) {
+      const offset = (currentPage - 1) * itemsPerPage;
+      fetchJobs("", offset, itemsPerPage);
+    }
+  }, [currentPage, fetchJobs, itemsPerPage, loading]);
 
   const totalPages = Math.ceil(jobs.length / itemsPerPage);
 
   return {
+    jobs,
     currentPage,
     setCurrentPage,
     totalPages,
