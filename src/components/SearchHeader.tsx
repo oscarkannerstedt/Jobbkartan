c0674377163cc57bad97ff5f338e18e284c8baa6
Annika-Nyholm <146995394+Annika-Nyholm@users.noreply.github.com>
2024-10-08 17:16:06 +0200

    💡 commenting

--- src/components/SearchHeader.tsx
+++ src/components/SearchHeader.tsx
@@ -8,30 +8,31 @@ import {
 	LayoutContainerVariation,
 } from '@digi/arbetsformedlingen';
 import {
-  DigiFormInputSearch,
-  DigiLayoutBlock,
-  DigiLayoutColumns,
-  DigiLayoutContainer,
-} from "@digi/arbetsformedlingen-react";
-import "../styles/searchHeader.css";
+	DigiFormInputSearch,
+	DigiLayoutBlock,
+	DigiLayoutColumns,
+	DigiLayoutContainer,
+} from '@digi/arbetsformedlingen-react';
+import '../styles/searchHeader.css';
 // import { useNavigate } from "react-router-dom";
-import { DigiFormInputSearchCustomEvent } from "@digi/arbetsformedlingen/dist/types/components";
-import { jobContext } from "../services/jobContext";
-import { useContext } from "react";
-import { JobMap } from "./JobMap";
-import PaginationContext from "../contexts/PaginationContext";
+import { DigiFormInputSearchCustomEvent } from '@digi/arbetsformedlingen/dist/types/components';
+import { jobContext } from '../services/jobContext';
+import { useContext } from 'react';
+import { JobMap } from './JobMap';
+import PaginationContext from '../contexts/PaginationContext';
+// import ScreenSizeContext from '../contexts/ScreenSizeContext';
 
 interface ISearchHeaderProps {
-  onSearch: (searchTerm: string) => void;
+	onSearch: (searchTerm: string) => void;
 }
 
 export const SearchHeader = ({ onSearch }: ISearchHeaderProps) => {
-  // const navigate = useNavigate();
-  const context = useContext(jobContext);
-  const paginationContext = useContext(PaginationContext);
+	// const navigate = useNavigate();
+	const context = useContext(jobContext);
+	const paginationContext = useContext(PaginationContext);
 	const zoomLevel = 4.5;
 
-	//const sizeContext = useContext(ScreenSizeContext);
+	// const sizeContext = useContext(ScreenSizeContext);
 
 	// if (!sizeContext) {
 	// 	throw new Error(
@@ -41,24 +42,24 @@ export const SearchHeader = ({ onSearch }: ISearchHeaderProps) => {
 
 	// const { isDesktop } = sizeContext;
 
-  const handleSearchSubmit = async (
-    event: DigiFormInputSearchCustomEvent<string>
-  ) => {
-    event.preventDefault();
+	const handleSearchSubmit = async (
+		event: DigiFormInputSearchCustomEvent<string>
+	) => {
+		event.preventDefault();
 
-    const term = event.detail;
+		const term = event.detail;
 
-    if (context) {
-      await context.fetchJobs(term);
-      onSearch(term);
-    }
+		if (context) {
+			await context.fetchJobs(term);
+			onSearch(term);
+		}
 
-    if (paginationContext) {
-      paginationContext.setCurrentPage(1);
-    }
+		if (paginationContext) {
+			paginationContext.setCurrentPage(1);
+		}
 
-    // navigate(`/annonser`, { state: { searchTerm: term } });
-  };
+		// navigate(`/annonser`, { state: { searchTerm: term } });
+	};
 
 	return (
 		// <div id='search-header'>
@@ -99,25 +100,41 @@ export const SearchHeader = ({ onSearch }: ISearchHeaderProps) => {
 				</DigiLayoutColumns>
 			</DigiLayoutContainer>
 			{/* ) : (
-					<DigiLayoutContainer className='container-content'>
-						<h1 className='search-bar-heading'>Jobbkartan</h1>
-						<p className='search-bar-text'>
-							Lediga jobb för hela sverige
-						</p>
-						<DigiFormInputSearch
-							afLabel='Sök på ett eller flera ord'
-							afVariation={FormInputSearchVariation.MEDIUM}
-							afType={FormInputType.SEARCH}
-							afButtonText='Sök'
-							afLabelDescription='Skriv t.e.x frontend örebro'
-							onAfOnSubmitSearch={handleSearchSubmit}
-							id='search'
-						></DigiFormInputSearch>
-						<div className='map-container'>
-							<JobMap zoomLevel={zoomLevel}/>
-						</div>
-					</DigiLayoutContainer>
-				)} */}
+				<DigiLayoutContainer
+					afVariation={LayoutContainerVariation.STATIC}
+				>
+					<DigiLayoutColumns
+						afElement={LayoutColumnsElement.DIV}
+						afVariation={LayoutColumnsVariation.TWO}
+						className='column-layout'
+					>
+						<DigiLayoutContainer className='container-content'>
+							<div className='search-title'>
+								<h1 className='search-bar-heading'>
+									Jobbkartan
+								</h1>
+								<p className='search-bar-text'>
+									Lediga jobb för hela sverige
+								</p>
+							</div>
+							<DigiFormInputSearch
+								afLabel='Sök på ett eller flera ord'
+								afVariation={FormInputSearchVariation.MEDIUM}
+								afType={FormInputType.SEARCH}
+								afButtonText='Sök'
+								afLabelDescription='Skriv t.e.x frontend örebro'
+								onAfOnSubmitSearch={handleSearchSubmit}
+								id='search'
+							></DigiFormInputSearch>
+						</DigiLayoutContainer>
+						<DigiLayoutContainer>
+							<div className='map-container'>
+								<JobMap zoomLevel={zoomLevel}></JobMap>
+							</div>
+						</DigiLayoutContainer>
+					</DigiLayoutColumns>
+				</DigiLayoutContainer>
+			)} */}
 		</DigiLayoutBlock>
 		// </div>
 	);
