cfc29dff1c63c3327e01d2e56e02049e8da84c25
Aanndreas <andreas.lundin@hotmail.com>
2024-10-09 10:28:16 +0200

    💄 Added img of map + Initial 'setup' of home-page-styling (with purple bg)

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,19 +1,39 @@
+import {
+	LayoutBlockContainer,
+	LayoutBlockVariation,
+} from "@digi/arbetsformedlingen";
+import {
+	DigiLayoutBlock,
+	DigiLayoutContainer,
+	DigiTypography,
+} from "@digi/arbetsformedlingen-react";
 import { useNavigate } from "react-router-dom";
 
+import "../styles/home.css";
+
 const Home = () => {
-  const navigate = useNavigate();
+	const navigate = useNavigate();
 
-  const handleNavigate = () => {
-    navigate("/annonser"); // Ersätt "/annonser" med den faktiska vägen till PrintAllJobs om den är annan
-  };
+	const handleNavigate = () => {
+		navigate("/annonser"); // Ersätt "/annonser" med den faktiska vägen till PrintAllJobs om den är annan
+	};
 
-  return (
-    <div className="home-container">
-      <h1>Välkommen till Jobbkartan!</h1>
-      <p>Klicka på knappen nedan för att se alla jobbannonser.</p>
-      <button onClick={handleNavigate}>Visa Jobbannonser</button>
-    </div>
-  );
+	return (
+		<div id="home-container">
+			<DigiLayoutBlock
+				afVariation={LayoutBlockVariation.TRANSPARENT}
+				afContainer={LayoutBlockContainer.STATIC}
+			>
+				<DigiTypography>
+					<div className="home-container">
+						<h1>Välkommen till Jobbkartan!</h1>
+						<p>Klicka på knappen nedan för att se alla jobbannonser.</p>
+						<button onClick={handleNavigate}>Visa Jobbannonser</button>
+					</div>
+				</DigiTypography>
+			</DigiLayoutBlock>
+		</div>
+	);
 };
 
 export default Home;
