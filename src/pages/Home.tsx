22ecf9c60f2fdc1bdabf59c5665524a255963344
Aanndreas <andreas.lundin@hotmail.com>
2024-10-09 12:15:18 +0200

    🎨 🚧 Continous improvements to code formatting etc

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,8 +7,8 @@ import {
 import {
 	DigiLayoutBlock,
 	DigiLayoutColumns,
+	DigiLayoutContainer,
 	DigiMediaImage,
-	DigiTypography,
 } from "@digi/arbetsformedlingen-react";
 import { useNavigate } from "react-router-dom";
 
@@ -23,35 +23,34 @@ const Home = () => {
 
 	return (
 		<div id="home-container">
-			<DigiLayoutColumns
-				afElement={LayoutColumnsElement.DIV}
-				afVariation={LayoutColumnsVariation.TWO}
+			<DigiLayoutBlock
+				afVariation={LayoutBlockVariation.TRANSPARENT}
+				afContainer={LayoutBlockContainer.STATIC}
 			>
-				<DigiLayoutBlock
-					afVariation={LayoutBlockVariation.TRANSPARENT}
-					afContainer={LayoutBlockContainer.STATIC}
+				<DigiLayoutColumns
+					afElement={LayoutColumnsElement.DIV}
+					afVariation={LayoutColumnsVariation.TWO}
 				>
-					<DigiTypography>
-						<div className="home-container">
-							<h1 className="welcome-text">Välkommen till Jobbkartan!</h1>
-							<DigiMediaImage
-								className="map-img-start"
-								afUnlazy
-								afHeight="500"
-								afWidth="750"
-								afSrc="./src/assets/karta_startsidan.png"
-								afAlt="Karta över lediga jobb i Sverige"
-							></DigiMediaImage>
-						</div>
-						<div className="home-lower-sub-container">
-							<p className="home-body-text">
-								Klicka på knappen nedan för att se alla jobbannonser.
-							</p>
-							<button onClick={handleNavigate}>Visa Jobbannonser</button>
-						</div>
-					</DigiTypography>
-				</DigiLayoutBlock>
-			</DigiLayoutColumns>
+					<DigiLayoutContainer>
+						<h1 className="welcome-text">Välkommen till Jobbkartan!</h1>
+					</DigiLayoutContainer>
+					<DigiLayoutContainer>
+						<DigiMediaImage
+							className="map-img-start "
+							afUnlazy
+							afFullwidth={true}
+							afSrc="./src/assets/karta_startsidan.png"
+							afAlt="Karta över lediga jobb i Sverige"
+						></DigiMediaImage>
+					</DigiLayoutContainer>
+					<div className="home-lower-sub-container">
+						<p className="home-body-text">
+							Klicka på knappen nedan för att se alla jobbannonser.
+						</p>
+						<button onClick={handleNavigate}>Visa Jobbannonser</button>
+					</div>
+				</DigiLayoutColumns>
+			</DigiLayoutBlock>
 		</div>
 	);
 };
